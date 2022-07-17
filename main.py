c0d0792ff33feef57f0ba9ac574105cb610c920f
RazvanBanateanu <banateanu.razvan1@gmail.com>
2022-07-17 16:28:34 +0300

    snake&food

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import width
 import pygame
 import time
+import random
 
 if __name__ == "__main__":
     pygame.init()
@@ -11,20 +12,44 @@ if __name__ == "__main__":
     pygame.display.set_caption("The Snake")
 
     x, y = 200, 200
-    delta_x, delta_y = 0, 0
+    delta_x, delta_y = 10, 0
+    food_x = random.randrange(0, width)//10*10
+    food_y = random.randrange(0, height)//10*10
+
+    body = [(x, y)]
 
     clock = pygame.time.Clock()
 
+    game_over = False
+
     def snake():
-        global x, y
+        global x, y, food_x, food_y, game_over
         x=(x+delta_x)%width
         y=(y+delta_y)%height
+
+        if((x,y)) in body:
+            game_over = True
+            return
+
+        body.append((x, y)) 
+        if(food_x == x and food_y == y):
+            while((food_x, food_y) in body):
+                food_x, food_y = random.randrange(0,width)//10*10, random.randrange(0, height)//10*10
+            else:
+                del body[0]    
+
         surface.fill((255, 255, 255))
-        pygame.draw.rect(surface, (0, 0, 0), [x, y, 10, 10] )
+        pygame.draw.rect(surface, (0, 255, 0), [food_x, food_y, 10, 10])
+        for (i,j) in body:
+            pygame.draw.rect(surface, (0, 0, 0), [x, y, 10, 10] )
         pygame.display.update()
 
 
     while True:
+        if(game_over):
+            pygame.quit()
+            quit()
+            
         events = pygame.event.get()
         for event in events:
             if(event.type == pygame.QUIT):
@@ -33,20 +58,24 @@ if __name__ == "__main__":
 
             if(event.type == pygame.KEYDOWN):
                 if(event.key == pygame.K_LEFT):
-                    delta_x = -10
+                    if delta_x != 10:
+                        delta_x = -10
                     delta_y = 0
                 
                 elif(event.key == pygame.K_RIGHT):
-                    delta_x = 10
+                    if delta_x != -10:
+                        delta_x = 10
                     delta_y = 0
 
                 elif(event.key == pygame.K_UP):
                     delta_x = 0
-                    delta_y = -10
+                    if delta_y != 10:
+                        delta_y = -10
 
                 elif(event.key == pygame.K_DOWN):
                     delta_x = 0
-                    delta_y = 10
+                    if delta_y != -10:
+                        delta_y = 10
                 
                 else:
                     continue
